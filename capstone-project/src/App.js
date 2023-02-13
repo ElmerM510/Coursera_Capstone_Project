698c68f4b759e2e08460653c2bd85cdc46cb7092
elmer martinez <elmermartinez52@gmail.com>
2023-02-13 09:07:44 -0800

    Made changes to index.html and added a nav component

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import React from 'react';
+//import { ReactFragment } from 'react';
+import Nav from "./components/Nav";
 
 function App() {
-  return (
-    <div className="App">
-     Homepage
-    </div>
-  );
+  return(
+      <Nav/>
+  )
 }
-
 export default App;
